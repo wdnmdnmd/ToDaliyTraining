e424eef0407365c686736a39babb577e39584825
jia.yuan <jia.yuan@samsung.com>
2024-02-27 18:32:41 +0800

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
+        System.out.println(1);
     }
 }
 
